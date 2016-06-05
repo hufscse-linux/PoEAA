b5a36beb0123ddd32717266412234940c94ed664
Dongseob Park <dongseob.park@gmail.com>
2016-06-06 02:18:43 +0900

    refactoring: hibernate fixture

---
+++ java/src/test/java/example/domain/HibernateFixture.java
@@ -0,0 +1,35 @@
+package example.domain;
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class HibernateFixture {
+  public static final SessionFactory sessionFactory;
+
+  public interface SessionHandler {
+    public void work(Session session);
+  };
+  
+  public static void inSession(SessionHandler handler) {
+    Session session = sessionFactory.openSession();
+    if(handler != null) handler.work(session);
+    session.close();
+  }
+  
+  static {
+    Configuration configuration = new Configuration();
+    configuration.addAnnotatedClass(Student.class);
+    configuration.addAnnotatedClass(Department.class);
+    configuration.addAnnotatedClass(Professor.class);
+    configuration.addAnnotatedClass(Course.class);
+
+    configuration.setProperty("hibernate.dialect",
+                              "org.hibernate.dialect.SQLiteDialect");
+    configuration.setProperty("hibernate.connection.driver_class",
+                              "org.sqlite.JDBC");
+    configuration.setProperty("hibernate.connection.url",
+                              "jdbc:sqlite:university.sqlite3");
+    
+    sessionFactory = configuration.buildSessionFactory();
+  }
+};
--- java/src/test/java/example/domain/StudentTest.java
+++ java/src/test/java/example/domain/StudentTest.java
@@ -3,45 +3,24 @@ package example.domain;
 import java.util.*;
 import org.junit.*;
 import org.hibernate.*;
-import org.hibernate.cfg.*;
+
+import static example.domain.HibernateFixture.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class StudentTest {
-  private SessionFactory sessionFactory;
-  private Session session = null;
-
-  @Before
-  public void before() {
-    // setup the session factory
-    Configuration configuration = new Configuration();
-    configuration.addAnnotatedClass(Student.class);
-    configuration.addAnnotatedClass(Department.class);
-    configuration.addAnnotatedClass(Professor.class);
-    configuration.addAnnotatedClass(Course.class);
-
-    configuration.setProperty("hibernate.dialect",
-                              "org.hibernate.dialect.SQLiteDialect");
-    configuration.setProperty("hibernate.connection.driver_class",
-                              "org.sqlite.JDBC");
-    configuration.setProperty("hibernate.connection.url",
-                              "jdbc:sqlite:university.sqlite3");
-    
-    sessionFactory = configuration.buildSessionFactory();
-    session = sessionFactory.openSession();
-  }
-  @After
-  public void after() {
-    session.close();
-    sessionFactory.close();
-  }
   @Test
   public void loadStudents() {
-    List<Student> students = session.createCriteria(Student.class).list();
+    inSession(
+        new SessionHandler() {
+          public void work(Session session) {
+            List<Student> students = session.createCriteria(Student.class).list();
 
-    Student student = students.get(0);
-    System.out.println("Student name: " + student.getName() + ", department: " + student.getDepartment().getName());
+            Student student = students.get(0);
+            System.out.println("Student name: " + student.getName() + ", department: " + student.getDepartment().getName());
 
-    assertThat(students.isEmpty(), is(false));
+            assertThat(students.isEmpty(), is(false));
+          }
+        });
   }
 };
