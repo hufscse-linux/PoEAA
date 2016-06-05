b652285e678b0a115770e812db754fd93a90fa91
Dongseob Park <dongseob.park@gmail.com>
2016-06-05 17:28:59 +0900

    revised assignment

--- assignments/mapper/concepts.py
+++ assignments/mapper/concepts.py
@@ -2,6 +2,10 @@
 
 from pprint import pprint
 
+class Department(object):
+    def __init__(self, name):
+        self.name = name
+
 class Student(object):
 
     def __init__(self, name, dept):
@@ -14,9 +18,12 @@ class StudentMapper(object):
         pass
 
 def sql_result_from_database():
+    # read university.sqlite3 and select student information
     pass
 
 result = sql_result_from_database()
 student = StudentMapper.map_to_object(result)
 
 pprint(student) # output is mapped result from database
+
+print("Student - name: %s, dept: %s" % (student.name, student.dept.name))
