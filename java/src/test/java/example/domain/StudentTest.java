20de6320dcc54271af65d4a2649baeea92deef3d
Dongseob Park <dongseob.park@gmail.com>
2016-06-06 18:02:32 +0900

    minor fix

--- java/src/test/java/example/domain/StudentTest.java
+++ java/src/test/java/example/domain/StudentTest.java
@@ -11,10 +11,9 @@ import static org.hamcrest.CoreMatchers.*;
 public class StudentTest {
   @Test
   public void students_should_have_department() {
-    inSession(
-        session -> {
-          List<Student> students = session.createCriteria(Student.class).list();
-          students.forEach(student -> assertThat(student.getDepartment(), is(notNullValue())));
-        });
+    inSession(session -> {
+        List<Student> students = session.createCriteria(Student.class).list();
+        students.forEach(student -> assertThat(student.getDepartment(), is(notNullValue())));
+      });
   }
 };
