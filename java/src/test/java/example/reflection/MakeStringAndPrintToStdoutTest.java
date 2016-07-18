deb64254272e5fd93b899a815f23731e6e1cd83c
SunKyu <leesunkeuy2@gmail.com>
2016-07-19 00:10:19 +0900

    #11 use SystemRule

--- java/src/test/java/example/reflection/MakeStringAndPrintToStdoutTest.java
+++ java/src/test/java/example/reflection/MakeStringAndPrintToStdoutTest.java
@@ -3,11 +3,19 @@ package example.reflection;
 import java.lang.reflect.*;
 
 import org.junit.*;
+import java.io.PrintStream;
+import java.io.OutputStream;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import org.junit.contrib.java.lang.system.*;
 
 public class MakeStringAndPrintToStdoutTest {
+
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
     @Test
     public void test1() throws Exception {
         Class<?> string_class = Class.forName("java.lang.String");
@@ -17,10 +25,12 @@ public class MakeStringAndPrintToStdoutTest {
         assertThat("some string", is(str));
 
         Class<?> system_class = Class.forName("java.lang.System");
+
         Field out_field  = system_class.getField("out");
         Object out = out_field.get(null);
         Class<?> out_class = out.getClass();
         Method println = out_class.getMethod("println", String.class);
         println.invoke(out, str);
+        assertEquals(str+"\n", systemOutRule.getLog());
     }
 }
