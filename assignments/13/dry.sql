c3cd37f5f19fe1d9c7853150a594c3d5aede3f77
SunKyu <leesunkeuy2@gmail.com>
2016-08-04 21:52:44 +0900

    implement empolyee mapper
    insert empolyee data to sqlite3
    and implement empolyee mapper
    
    See also : #13, #14, #15, #16

--- assignments/13/dry.sql
+++ assignments/13/dry.sql
@@ -18,8 +18,8 @@ CREATE TABLE POSITION (
 CREATE TABLE EMPOLYEE (
     Name varchar(20),
     En integer primary key,
-    TeamId integer,
-    ProjectId integer,
+    TeamId integer default NULL,
+    ProjectId integer default NULL,
     PositionId integer,
     foreign key(TeamId) references TEAM(id),
     foreign key(ProjectId) references PROJECT(id),
@@ -53,3 +53,13 @@ INSERT into TEAM (TeamId, ProjectId, Name) VALUES(3, 3, "BFTT_TEAM");
 INSERT into TEAM (TeamId, ProjectId, Name) VALUES(4, 4, "YV_TEAM");
 INSERT into TEAM (TeamId, ProjectId, Name) VALUES(5, 5, "BG_TEAM");
 INSERT into TEAM (TeamId, ProjectId, Name) VALUES(6, 6, "BM_TEAM");
+
+
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("SunKyu", 1, 1, 1, 3);
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("KG", 2, 1, 1, 2);
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("Yong Hee", 3, 1, 1, 3);
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("SM", 4, 5, 4, 4);
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("HM", 5, 5, 5, 3);
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("JD", 6, 2, 2, 4);
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("SB", 7, 3, 3, 3);
+Insert into EMPOLYEE (Name, En, TeamId, ProjectId, PositionId) VALUES("SH", 8, 6, 6, 2);
